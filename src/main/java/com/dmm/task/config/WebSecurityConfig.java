5624ce7e07f377a6a94a8fce8238cc3793a076b1
Administrator <Administrator@host.docker.internal>
2025-02-07 20:33:30 +0900

    認証成功時に遷移するデフォルトのパス

--- src/main/java/com/dmm/task/config/WebSecurityConfig.java
+++ src/main/java/com/dmm/task/config/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// 認可の設定
-
+		
 		http.authorizeRequests().antMatchers("/loginForm").permitAll() // loginFormは、全ユーザからのアクセスを許可
 		.anyRequest().authenticated(); // loginForm以外は、認証を求める
 
@@ -43,9 +43,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.loginProcessingUrl("/authenticate") // フォーム認証処理のパス
 		.usernameParameter("userName") // ユーザ名のリクエストパラメータ名
 		.passwordParameter("password") // パスワードのリクエストパラメータ名
-		.defaultSuccessUrl("/home") // 認証成功時に遷移するデフォルトのパス
+		.defaultSuccessUrl("/main") // 認証成功時に遷移するデフォルトのパス
 		.failureUrl("/loginForm?error=true"); // 認証失敗時に遷移するパス
-
+		
 		// ログアウト設定
 		http.logout().logoutSuccessUrl("/loginForm") // ログアウト成功時に遷移するパス
 		.permitAll(); // 全ユーザに対して許可
